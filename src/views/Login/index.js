6b4283c34d1975a5320fc3ae1c7663f28066d9c5
Anuj Panwar <anuj.panwar.ch.ap@gmail.com>
2022-01-28 20:56:42 +0530

    new changes

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -13,7 +13,9 @@ export default class Login extends Component {
 		this.state ={
 		email:'',
 		password:'',
-
+		emailError:'',
+		passwordError:'',
+		loginError:'',
 		}
 	}
 	onChangeEmail(e){
@@ -27,8 +29,47 @@ export default class Login extends Component {
 			password:e.target.value
 		});
 	}
+	validate() {
+		// let firstNameError= '';
+		// let lastNameError= '';
+		//  let emailError= '';
+		let passwordError = '';
+		let emailError = '';
+		this.setState({loginError:''});
+		if (this.state.email == '') {
+		    emailError = 'Please enter a E-mail'
+		}else{
+			emailError = '';
+		}
+		if (this.state.password == '' || this.state.password.length < 6) {
+		    passwordError = 'Please enter a valid password';
+		}else{
+			passwordError = '';
+		}
+		if (emailError) {
+		  this.setState({ emailError })
+		  return false
+		}else{
+		this.setState({
+			emailError:''
+		});	
+		}
+
+		if (passwordError) {
+		  this.setState({ passwordError })
+		  return false
+		}else{
+			this.setState({
+				passwordError:''
+			});
+		}
+
+		return true;
+	  }
 	onSubmit(e){
 	  e.preventDefault();
+	  const isValid = this.validate()
+    if (isValid) {
 		const obj ={
 		  email:this.state.email,
 		  password:this.state.password,
@@ -36,8 +77,14 @@ export default class Login extends Component {
 	   
 		axios.post('http://localhost/samreact/login.php',obj)
 		.then(res=> {
-			console.log(res.data);
-			sessionStorage.setItem('userData',JSON.stringify(res.data));
+			let result = res.data;
+			if(result.type=='success'){
+				this.setState({loginError:'Login Successful'});
+				sessionStorage.setItem('userData',JSON.stringify(result.user));
+			}else{
+				this.setState({loginError:'Email or Password not correct'});
+			}
+			
 		})
 		.catch(error => {
 		  console.log(error.response)
@@ -49,7 +96,7 @@ export default class Login extends Component {
 		password:'',
 	 
 	  })
-	   
+	}  
 	}
   render() {
     return (
@@ -70,14 +117,16 @@ export default class Login extends Component {
                                     <h4 className="text-dark-50 text-center mt-0 font-weight-bold">Sign In</h4>
                                     <p className="text-muted mb-4">Enter your email address and password to access admin panel.</p>
                                 </div>
+								<div className="loginmessage">{this.state.loginError}</div>
                                 <form action="#">
                                     <div className="form-group">
-                                        <label for="emailaddress">Email address</label>
+                                        <label htmlFor="emailaddress">Email address</label>
                                         <input onChange={this.onChangeEmail} value={this.state.email} className="form-control" type="email" id="emailaddress" required="" placeholder="Enter your email"/>
+										<span className="error">{this.state.emailError}</span>
                                     </div>
                                     <div className="form-group">
                                         <a href="pages-recoverpw.html" className="text-muted float-right"><small>Forgot your password?</small></a>
-                                        <label for="password">Password</label>
+                                        <label htmlFor="password">Password</label>
                                         <div className="input-group input-group-merge">
                                             <input type="password" onChange={this.onChangePassword} value={this.state.password} id="password" className="form-control" placeholder="Enter your password"/>
                                             <div className="input-group-append" data-password="false">
@@ -86,11 +135,12 @@ export default class Login extends Component {
                                                 </div>
                                             </div>
                                         </div>
+										<span className="error">{this.state.passwordError}</span>
                                     </div>
                                     <div className="form-group mb-3">
                                         <div className="custom-control custom-checkbox">
                                             <input type="checkbox" className="custom-control-input" id="checkbox-signin" checked/>
-                                            <label className="custom-control-label" for="checkbox-signin">Remember me</label>
+                                            <label className="custom-control-label" htmlFor="checkbox-signin">Remember me</label>
                                         </div>
                                     </div>
                                     <div className="form-group mb-0 text-center">
--- src/views/Register/index.js
+++ src/views/Register/index.js
@@ -20,7 +20,10 @@ export default class Register extends Component {
 		name: '',
 		email:'',
 		password:'',
-
+		nameError: '',
+		emailError:'',
+		passwordError:'',
+		registerError:'',
 		}
 	}
 	onChangeName(e){
@@ -40,29 +43,82 @@ export default class Register extends Component {
 			password:e.target.value
 		});
 	}
+	validate() {
+		
+		let nameError = '';
+		let emailError = '';
+		let passwordError = '';
+		
+		if (this.state.name == '') {
+		  nameError = 'Please enter your name'
+		}
+		if (this.state.email == '') {
+		  emailError = 'Please enter a E-mail'
+		}
+		if (this.state.password == '' || this.state.password.length < 6) {
+		  passwordError = 'Please enter a valid password'
+		}
+		if (nameError) {
+		  this.setState({ nameError })
+		  return false
+		}else{
+		this.setState({
+			nameError:''
+		});	
+		}
+		if (emailError) {
+		  this.setState({ emailError })
+		  return false
+		}else{
+		this.setState({
+			emailError:''
+		});	
+		}
+
+		if (passwordError) {
+		  this.setState({ passwordError })
+		  return false
+		}else{
+			this.setState({
+				passwordError:''
+			});
+		}
+
+		return true;
+	  }
 	onSubmit(e){
 	  e.preventDefault();
-		const obj ={
-		  name:this.state.name,
-		  email:this.state.email,
-		  password:this.state.password,
-		};
-	   
-		axios.post('http://localhost/samreact/insert.php',obj)
-		.then(res=> console.log(res.data))
-		.catch(error => {
-		  console.log(error.response)
-	  });
-	 
-	//To clear text box values 
-	  this.setState({
-		name: '',
-		email:'',
-		phone:'',
-		password:'',
-	 
-	  })
-	   
+	  const isValid = this.validate()
+		if (isValid) {
+			const obj ={
+			  name:this.state.name,
+			  email:this.state.email,
+			  password:this.state.password,
+			};
+		   
+			axios.post('http://localhost/samreact/register.php',obj)
+			.then(res=> {
+				let result = res.data;
+				if(result.type=='success'){
+					this.setState({registerError:result.message});
+					//sessionStorage.setItem('userData',JSON.stringify(result.user));
+				}else{
+					this.setState({registerError:result.message});
+				}
+				
+			})
+			.catch(error => {
+			  console.log(error.response)
+		  });
+		 
+		//To clear text box values 
+		  this.setState({
+			name: '',
+			email:'',
+			password:'',
+		 
+		  })
+		}   
 	}
   render() {
     return (
@@ -86,17 +142,19 @@ export default class Register extends Component {
                                     <h4 className="text-dark-50 text-center mt-0 font-weight-bold">Free Sign Up</h4>
                                     <p className="text-muted mb-4">Don't have an account? Create your account, it takes less than a minute </p>
                                 </div>
-
+								<div className="loginmessage">{this.state.registerError}</div>
                                 <form method="POST" action="#">
 
                                     <div className="form-group">
                                         <label for="fullname">Full Name</label>
                                         <input onChange={this.onChangeName} className="form-control" type="text" id="fullname" placeholder="Enter your name" value={this.state.name} required/>
+										<span className="error">{this.state.nameError}</span>
                                     </div>
 
                                     <div className="form-group">
                                         <label for="emailaddress">Email address</label>
                                         <input onChange={this.onChangeEmail} className="form-control" type="email" id="emailaddress" required value={this.state.email} placeholder="Enter your email"/>
+										<span className="error">{this.state.emailError}</span>
                                     </div>
 
                                     <div className="form-group">
@@ -109,6 +167,7 @@ export default class Register extends Component {
                                                 </div>
                                             </div>
                                         </div>
+										<span className="error">{this.state.passwordError}</span>
                                     </div>
 
                                     <div className="form-group">
